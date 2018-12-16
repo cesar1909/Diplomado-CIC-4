a176b98ddd75571f323ca57bcd4667191b66f32f
Manuel Pérez <jmperezsantos@gmail.com>
2018-12-15 18:01:03 -0600

    CRUD 50% User

--- SpringExample/src/main/java/mx/ipn/cic/SpringExample/controller/UserController.java
+++ SpringExample/src/main/java/mx/ipn/cic/SpringExample/controller/UserController.java
@@ -1,22 +1,40 @@
 package mx.ipn.cic.SpringExample.controller;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import mx.ipn.cic.SpringExample.model.UserModel;
+import mx.ipn.cic.SpringExample.services.IUserService;
 
 @Controller
 @RequestMapping(path = "/user")
 public class UserController {
 
+	@Autowired
+	private IUserService userService;
+
 	private Log logger = LogFactory.getLog(UserController.class);
+
+	@PostMapping("/register")
+	public String userSubmit(@Validated @ModelAttribute(name = "user") UserModel user) {
+
+		logger.info(user);
+
+		this.userService.create(user);
+
+		return "redirect:/user/all";
+	}
 
 	@GetMapping(path = "/userForm")
 	public ModelAndView getForm() {
@@ -27,17 +45,38 @@ public class UserController {
 
 	}
 
-	@PostMapping("/register")
-	public String userSubmit(
-			@Validated @ModelAttribute(name = "user") UserModel user
+	@GetMapping(path = "/all")
+	public ModelAndView getAll() {
+
+		List<UserModel> userList = this.userService.findAll();
+
+		ModelAndView mav = new ModelAndView("user_list");
+		mav.addObject("users", userList);
+
+		return mav;
+
+	}
+	
+	@GetMapping(path="/{id}/delete")
+	public String delete(
+			@PathVariable(name="id") int id
 			) {
-
-		logger.info(user);
-
-		return "success";
+		
+		this.userService.deleteById(id);
+		
+		return "redirect:/user/all";
+		
 	}
 
 }
+
+
+
+
+
+
+
+
 
 
 
